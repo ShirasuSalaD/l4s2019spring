5c7bb60984e57b5df7adc165acd79e23d60ba50d
ShirasuSalaD <hihihipoke@gmail.com>
2019-02-06 00:25:23 +0900

    04 JSON配列 DONE

--- express/app.rb
+++ express/app.rb
@@ -1,3 +1,24 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+get '/' do
+  erb :form
+end
+
+get '/list' do
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({
+    method: "getStations",
+    x: params[:x],
+    y: params[:y]
+  })
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  @stations = json["response"]["station"]
+  erb :list
+end
