ace782f88084a1042048f17acfbe1b2019ee4c0e
ShirasuSalaD <hihihipoke@gmail.com>
2019-02-05 23:06:09 +0900

    002 Sinatra入門 DONE

--- wikipedia/app.rb
+++ wikipedia/app.rb
@@ -1,3 +1,13 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'net/http'
+require 'uri'
+
+get '/:keyword' do
+  base_url = 'http://wikipedia.simpleapi.net/api?output=html&keyword='
+  keyword = params[:keyword]
+  url = URI.parse(base_url+keyword)
+  Net::HTTP.get(url)
+end
