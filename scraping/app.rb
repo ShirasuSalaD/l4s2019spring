96a385d14415c35c2a6a16635dd5923faac718e3
ShirasuSalaD <hihihipoke@gmail.com>
2019-02-14 10:45:49 +0900

    010 スクレイピング　webarchiveのところの正規表現に詰まった

--- scraping/app.rb
+++ scraping/app.rb
@@ -1,3 +1,16 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'open-uri'
+require 'nokogiri'
+
+get '/' do
+  url = 'http://lit.gahaku.tech/old/'
+  html = open(url).read
+  parsed_html = Nokogiri::HTML.parse(html,nil,'utf-8')
+  parsed_html.css('ul.supporter-list').css('li').each do |node|
+    anchor = node.css('a')
+    logger.info anchor.attribute('href').value.match(/https://web\.archive\.org/web/20170603211131/https?:\/\/(?:www\.)?(?:jp\.)?(?:info\.)?(?:docs\.)?([^\.]*)/)[1]
+  end
+end
